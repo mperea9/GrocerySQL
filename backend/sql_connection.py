50a1305e3494a43060ad2938821ed9db77d9d7c5
mperea9 <mperea97@gmail.com>
2021-06-30 00:28:44 -0600

    Project Upload

---
+++ backend/sql_connection.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+__cnx = None
+
+
+def get_sql_connection():
+    global __cnx
+    if __cnx is None:
+        __cnx = mysql.connector.connect(user='root', password='password',
+                                        host='127.0.0.1',
+                                        database='GroceryInventory')
+
+    return __cnx
