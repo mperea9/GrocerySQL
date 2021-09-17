4f462f863aee123e349ee43c2b986d2a08953de2
Mel Perea <mel.perea97@gmail.com>
2021-09-16 22:01:03 -0600

    Update sql_connection.py

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -6,7 +6,7 @@ __cnx = None
 def get_sql_connection():
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='password',
+        __cnx = mysql.connector.connect(user='---', password='-----',
                                         host='127.0.0.1',
                                         database='GroceryInventory')
 
